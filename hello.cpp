f64781fc03cb638a67e5fe03067a89ad3cee4f5e
Sofia Klementschitz <sofia.klementschitz@bulme.at>
2021-10-06 10:37:51 +0200

    Interactivity

--- hello.cpp
+++ hello.cpp
@@ -2,8 +2,14 @@
 
 #include <iostream>
 
+using namespace std;
+
 int main ()
 {
-	std::cout << "Hallo 4BHEL!" <<std::endl;
-	return 0
+	string name;
+	cout << "Hallo 4BHEL!" << endl;
+	cout << "Dein Name: ";
+	cin >> name;
+	cout << "Hallo " << name << endl;
+	return 0;
 }
